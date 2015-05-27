f2fdf97a014639fcd8cf4901a3932a24bd4d7bb4
Almir Jr <mirjahal@hotmail.com>
2015-05-26 22:19:29 -0300

    Sub-view cabeçalho

---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,35 @@
+<nav class="top-bar" data-topbar>
+	<ul class="title-area">
+		<li class="name">
+			<h1>
+				<a href="#">Laplata</a>
+			</h1>
+		</li>
+		<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+	</ul>
+
+	<section class="top-bar-section">
+		<ul class="left">
+			<li class="divider"></li>
+			<li><a href="#">Categoria</a></li>
+
+			<li class="divider"></li>
+			<li class="active"><a href="#">Conta bancária</a></li>
+
+			<li class="divider"></li>
+			<li><a href="#">Cartão</a></li>
+			<li class="divider"></li>
+		</ul>
+
+		<ul class="right">
+			<li class="has-dropdown">
+				<a href="#">Usuário</a>
+				<ul class="dropdown">
+					<li><a href="#">Perfil</a></li>
+					<li class="divider"></li>
+					<li><a href="#">Sair →</a></li>
+				</ul>
+			</li>
+		</ul>
+	</section>
+</nav>
