3c621f039248e39def831e9ff9abab967e818b3c
Almir Jr <mirjahal@hotmail.com>
2015-05-26 22:18:50 -0300

    Sub-view rodapé

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="row">
+	<div class="large-12 columns">
+		<hr />
+		<div class="row">
+			<div class="large-6 columns">
+				<p>Laplata</p>
+			</div>
+
+			<div class="large-6 columns">
+				<ul class="inline-list right">
+					<li><a href="#">Categoria</a></li>
+					<li><a href="#">Conta bancária</a></li>
+					<li><a href="#">Cartão</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
