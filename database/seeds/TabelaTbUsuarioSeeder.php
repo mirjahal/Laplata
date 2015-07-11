061913f6c07486bdf0415734234c9e8cc3df6843
Almir Jr <mirjahal@hotmail.com>
2015-07-11 17:43:59 +0000

    Ajusta indentação

--- database/seeds/TabelaTbUsuarioSeeder.php
+++ database/seeds/TabelaTbUsuarioSeeder.php
@@ -5,24 +5,25 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Hash;
 
-class TabelaTbUsuarioSeeder extends Seeder {
+class TabelaTbUsuarioSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		DB::table('tb_usuario')->delete();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+     public function run()
+     {
+        DB::table('tb_usuario')->delete();
 
-		Usuario::create([
-			'nome' => 'José Almir',
-    		'sobrenome' => 'do Nascimento Júnior',
-			'email' => 'mirjahal@hotmail.com',
-			'senha' => Hash::make('123456'),
-			'status' => 'A'
-		]);
-	}
+        Usuario::create([
+            'nome' => 'José Almir',
+            'sobrenome' => 'do Nascimento Júnior',
+            'email' => 'mirjahal@hotmail.com',
+            'senha' => Hash::make('123456'),
+            'status' => 'A'
+        ]);
+    }
 
 }
